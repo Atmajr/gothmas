5724a6d1d329a4afdddafd5d8d5e8fbb552bc837
Justin Acker <justin@justinacker.com>
2019-12-20 22:59:36 -0500

    deploy to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'songs#index'
-  resources :songs, only: [:index, :show]
+  resources :songs, only: [:index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
