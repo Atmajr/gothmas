4809fc5e44f4b17d309a7db8e2550cd8d2414f0a
Justin Acker <justin@justinacker.com>
2019-12-21 01:05:27 -0500

    put double pipe back

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ApplicationRecord
 def self.search(search)
   if search
     search = search.to_str
-    Song.where("title like ? || artist like ?", "%#{search}%", "%#{search}%")
+    Song.where("title like ? or artist like ?", "%#{search}%", "%#{search}%")
     #find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
   else
     find(:all).limit(5)
